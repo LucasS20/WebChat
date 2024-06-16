b7ec7b271a4f6f51badfbd54ee9741da5eb94bb1
lucas <lucassr614@gmail.com>
2024-06-16 12:05:03 -0300

    tabuleiro

--- ng-chat-socket/src/app/components/telas/tabuleiro/tabuleiro.component.ts
+++ ng-chat-socket/src/app/components/telas/tabuleiro/tabuleiro.component.ts
@@ -1,29 +1,54 @@
 import {Component, OnInit} from '@angular/core';
 import {FormComponent} from "../../form/form/form.component";
 import {ActivatedRoute} from "@angular/router";
-import {JogoService} from "../../../services/jogoService/jogo.service";
 import {TabuleiroService} from "../../../services/tabuleiro.service";
+import {NgClass, NgForOf, NgOptimizedImage} from "@angular/common";
+import {ButtonComponent} from "../../inputs/button/button.component";
 
 @Component({
-  selector: 'app-tabuleiro',
-  standalone: true,
-  imports: [],
-  templateUrl: './tabuleiro.component.html',
-  styleUrl: './tabuleiro.component.scss'
+    selector: 'app-tabuleiro',
+    standalone: true,
+    imports: [
+        NgOptimizedImage,
+        NgForOf,
+        NgClass,
+        ButtonComponent
+    ],
+    templateUrl: './tabuleiro.component.html',
+    styleUrl: './tabuleiro.component.scss'
 })
 export class TabuleiroComponent extends FormComponent implements OnInit {
-  private userID: any;
-  private salaID: any;
+    private userID: any;
+    private salaID: any;
+    readonly numRows = 10;
+    readonly numCols = 5;
+    logoSrc = 'assets/logo_tis.png';
 
-  constructor(public route: ActivatedRoute,
-              public tabuleiroService: TabuleiroService) {
-    super();
-  }
+    constructor(public route: ActivatedRoute,
+                public tabuleiroService: TabuleiroService) {
+        super();
+    }
 
-  ngOnInit(): void {
-    this.userID = this.route.snapshot.params['userID'];
-    this.salaID = this.route.snapshot.params['salaID'];
-    this.tabuleiroService.conectarAosSockets(this.salaID);
-  }
+    ngOnInit(): void {
+        this.userID = this.route.snapshot.params['userID'];
+        this.salaID = this.route.snapshot.params['salaID'];
+        this.tabuleiroService.conectarAosSockets(this.salaID);
+    }
 
+    getNumber(row: number, col: number): string | number {
+        if (row === 0) {
+            return 1 + col;
+        } else if (col === this.numCols - 1 && row < this.numRows - 1) {
+            return 10 + row;
+        } else if (row === this.numRows - 1 && col > 0) {
+            return 14 + (this.numCols - col - 1);
+        } else if (col === 0 && row > 0) {
+            return 24 + (this.numRows - row - 2);
+        }
+        return '';
+    }
+
+    onButtonClick() {
+
+    }
 }
